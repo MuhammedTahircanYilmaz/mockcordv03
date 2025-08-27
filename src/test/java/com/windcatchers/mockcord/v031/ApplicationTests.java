88837cc58588309144e9e44a43aa691663b9e851
MuhammedTahircanYilmaz <mtahircanyilmaz@gmail.com>
2025-08-27 18:35:46 +0300

    Project initial commit

---
+++ src/test/java/com/windcatchers/mockcord/v031/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.windcatchers.mockcord.v031;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
